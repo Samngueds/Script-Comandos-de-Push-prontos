0e644fcb20a87c31d2b09bf64c5d4f8d5bf59963
Samuel Gueds <125109662+Samngueds@users.noreply.github.com>
2024-01-06 14:18:50 -0300

    Update automate_git.py
    Error, file HTML not upload: FIXED

--- GitAutomationScripts/automate_git.py
+++ GitAutomationScripts/automate_git.py
@@ -1,7 +1,7 @@
 import os
 
 # Absolute path of the directory
-directory = 'here-you-directory'
+directory = 'Here-your-Directory'
 
 # List only file names in the directory
 files = [file for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file))]
@@ -12,26 +12,37 @@ bat_file = os.path.join(directory, 'git_commands.bat')
 # Git commands
 git_commands = []
 
-# Add image files
-image_files = [file for file in files if any(extension in file for extension in ['.png', '.jpg', '.jpeg'])]
-if image_files:
-    git_commands.append(f'git add {" ".join([f"\"{file}\"" for file in image_files])} && git commit -m "Example image files"')
-
 # Add HTML files
 html_files = [file for file in files if file.endswith('.html')]
 if html_files:
-    git_commands.append(f'git add {" ".join([f"\"{file}\"" for file in html_files])} && git commit -m "Example HTML files"')
+    for html_file in html_files:
+        with open(os.path.join(directory, html_file), 'r', encoding='utf-8') as file:
+            # Extract the title from HTML (a simple approach)
+            lines = file.readlines()
+            for line in lines:
+                if '<title>' in line:
+                    title = line.strip().replace('<title>', '').replace('</title>', '')
+                    break
+            else:
+                title = 'Untitled'
+
+        git_commands.append(f'git add "{html_file}" && git commit -m "HTML file example: {title}"')
 
 # Add CSS files
 css_files = [file for file in files if file.endswith('.css')]
 if css_files:
-    git_commands.append(f'git add {" ".join([f"\"{file}\"" for file in css_files])} && git commit -m "Example CSS file"')
+    git_commands.append(f'git add {" ".join([f"\"{file}\"" for file in css_files])} && git commit -m "CSS files example"')
+
+# Add image files
+image_files = [file for file in files if any(extension in file for extension in ['.png', '.jpg', '.jpeg'])]
+if image_files:
+    git_commands.append(f'git add {" ".join([f"\"{file}\"" for file in image_files])} && git commit -m "Image files example"')
 
 # Add git push origin main command
 git_commands.append('git push origin main')
 
 # Write Git commands to the .bat file
-with open(bat_file, 'w') as f:
+with open(bat_file, 'w', encoding='utf-8') as f:
     f.write('\n'.join(git_commands))
 
 print(f'Script file generated: {bat_file}')
