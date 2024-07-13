343baa5e12934d208e501ed979b3bc2e15f5f50d
Samuel Gueds <125109662+Samngueds@users.noreply.github.com>
2024-07-13 09:57:48 -0300

    Update automate_git.py
    Add fucions and more files and extecion imagens

--- GitAutomationScripts/automate_git.py
+++ GitAutomationScripts/automate_git.py
@@ -1,7 +1,7 @@
 import os
 
-# Absolute path of the directory
-directory = 'Here-your-Directory'
+# Directory path
+directory = 'Your Directory here'
 
 files = [file for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file))]
 
@@ -9,29 +9,49 @@ bat_file = os.path.join(directory, 'git_commands.bat')
 
 git_commands = []
 
+# Function to add git commands and check if there are files
+def add_file_commands(extension, message):
+    files_of_type = [file for file in files if file.endswith(extension)]
+    if files_of_type:
+        git_commands.append(f'git add {" ".join([f"\"{file}\"" for file in files_of_type])} && git commit -m "{message}"')
+    else:
+        print(f'There are no {extension} files here')
+
+# HTML files
 html_files = [file for file in files if file.endswith('.html')]
 if html_files:
     for html_file in html_files:
         with open(os.path.join(directory, html_file), 'r', encoding='utf-8') as file:
-            
             lines = file.readlines()
             for line in lines:
                 if '<title>' in line:
                     title = line.strip().replace('<title>', '').replace('</title>', '')
                     break
             else:
-                title = 'Untitled'
+                title = 'No title'
+        git_commands.append(f'git add "{html_file}" && git commit -m "Example HTML file: {title}"')
+else:
+    print("There are no HTML files here")
 
-        git_commands.append(f'git add "{html_file}" && git commit -m "HTML file example: {title}"')
+# Add commands for other file types
+add_file_commands('.css', 'Example CSS files')
+add_file_commands('.js', 'Example JavaScript files')
+add_file_commands('.py', 'Example Python files')
+add_file_commands('.java', 'Example Java files')
+add_file_commands('.cpp', 'Example C++ files')
+add_file_commands('.c', 'Example C files')
+add_file_commands('.php', 'Example PHP files')
+add_file_commands('.rb', 'Example Ruby files')
+add_file_commands('.txt', 'Example text files')
 
-css_files = [file for file in files if file.endswith('.css')]
-if css_files:
-    git_commands.append(f'git add {" ".join([f"\"{file}\"" for file in css_files])} && git commit -m "CSS files example"')
-
-image_files = [file for file in files if any(extension in file for extension in ['.png', '.jpg', '.jpeg'])]
+# Image files
+image_files = [file for file in files if any(extension in file for extension in ['.png', '.jpg', '.jpeg', '.webp', '.ico'])]
 if image_files:
-    git_commands.append(f'git add {" ".join([f"\"{file}\"" for file in image_files])} && git commit -m "Image files example"')
+    git_commands.append(f'git add {" ".join([f"\"{file}\"" for file in image_files])} && git commit -m "Example image files"')
+else:
+    print("There are no image files here")
 
+# Command to push changes to the remote repository
 git_commands.append('git push origin main')
 
 with open(bat_file, 'w', encoding='utf-8') as f:
